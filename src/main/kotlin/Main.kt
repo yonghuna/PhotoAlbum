e710a0fb07bf46e351d6dec02f8cb76b5c7cef1c
YONGHUN <kooaas@daum.net>
2022-08-06 18:55:51 +0900

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+import java.lang.Exception
+import java.util.Scanner
+import javax.security.sasl.AuthorizeCallback
+
+fun main() {
+    val nums = arrayOf(12, 23, 52)
+    println(nums[0])
+    println(nums[1])
+    println(nums[2])
+}
+
+
+
+
+
+
+
